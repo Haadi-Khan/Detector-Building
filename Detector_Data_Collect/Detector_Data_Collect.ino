aa147f4adaaba5c1f281a16f8ef2271f7e62ca53
Haadi Khan <haadikhan05@gmail.com>
2021-08-23 19:31:45 -0400

    Initial Commit for 2021

--- Detector_Data_Collect/Detector_Data_Collect.ino
+++ Detector_Data_Collect/Detector_Data_Collect.ino
@@ -1,56 +1,33 @@
 #include <LiquidCrystal.h>
 
 //pins and LCD
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-#define THERMISTOR_PIN A0
-#define RED_LED 11
-#define GREEN_LED 12
-#define BLUE_LED 13
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+const int LED_PINS[3] = {11, 12, 13};
+#define DHT_PIN A0
 
 //Variables for Voltage Divider
-const int KNOWN_RESISTANCE = 1002;
-const int MAX_ADC = 1023;
-float sensorValue;
-
-//variables for thermistor volts, resistance, and temps
-float currentTemp, currentVoltage, thermResistance, thermAverage;
-
-//Variables for smoothing the readings
 #define READING_NUMBER 10
-float readings[READING_NUMBER];
-int total;
+const int KNOWN_RESISTANCE = 150;
+const float MAX_ADC = 1023.0;
+volatile int sensorValue;
+
+void lcdDisplay(float dhtResistance);
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(BLUE_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
-  lcd.begin(16, 2);
-
-  for(int thisReading = 0; thisReading < READING_NUMBER; thisReading++) {
-    readings[thisReading] = 0;
-  }
-}
-
-void lcdDisplay(double thermResistance, double currentVoltage) {
-  lcd.print((String)thermResistance + " Ohms");
-  lcd.setCursor(0, 1); //move to second row
-  lcd.print((String)currentVoltage + " Volts");
-  delay(1000);
-  lcd.clear();
+	Serial.begin(9600);
+	for(int i = 0; i < 3; i++) pinMode(LED_PINS[i],  OUTPUT);
+	lcd.begin(16, 2);
 }
 
 void loop() {
-  //Voltage Divider Equation
-  thermResistance = KNOWN_RESISTANCE * ((MAX_ADC/sensorValue) - 1);
+    float total = 0;
+    for(int i = 0; i < READING_NUMBER; i++) total += 5 * analogRead(DHT_PIN) / MAX_ADC;
+    float voltage = total/READING_NUMBER;
+    lcdDisplay(voltage);
+    delay(500);
+}
 
-  for (int i = 0; i >= READING_NUMBER; i++) {
-    total = total - readings[i];
-    readings[i] = analogRead(THERMISTOR_PIN);
-  }
-
-  float thermResistanceAverage = total/READING_NUMBER;
-  lcdDisplay(thermResistanceAverage, currentVoltage);
-
-  delay(100);
+void lcdDisplay(float voltage) {
+    lcd.print((String)voltage + " Volts");
+    lcd.clear();
 }
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Detector Building
 
-This is my code for the Science Olympiad Division C Detector Building event, I'm going to post my code on github for the project so I can store it here. This event got cancelled for the '20-'21 season, but I'm going to clean my garbage code for the '19-'20 season so I have something usable for the next time build/test events are allowed.
+This is my code for the Science Olympiad Division C Detector Building event.
 
 ## Detector Main
 
-This is the .ino file for the actual competition, just run it and you should be good to go
+This is the .ino file for the actual competition, just edit the ppm limits and you should be good to go
 
 ## Detector Data Collect
 
-This is a slightly modified file which disables some features (namely the led light blinking and temp readouts) to make data collection easier.
+This is a slightly modified file which disables some features (namely the led light blinking and ppm readouts) to make data collection easier.
